22dec544b34f6ef1bf5bdfb689b05c85aaffa26d
jeffgreenca <jeff.green.ca@gmail.com>
2019-12-22 21:36:45 -0800

    add compare script

---
+++ compare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ASM_1=~/Documents/nand2tetris/tools/Assembler.sh
+ASM_2=n2t-asm
+
+TARGETS=$(find ~/Documents/nand2tetris/projects/06/ -name \*.asm -type f)
+
+WORKDIR=${PWD}/temp
+mkdir ${WORKDIR} || rm -rf ${WORKDIR} && mkdir ${WORKDIR}
+
+for t in ${TARGETS}; do
+	fn=$(basename ${t})
+	cp ${t} ${WORKDIR}/${fn}
+	${ASM_1} ${WORKDIR}/${fn}
+	diff --color=always <(${ASM_2} ${WORKDIR}/${fn}) ${WORKDIR}/${fn:0:-4}.hack
+done
