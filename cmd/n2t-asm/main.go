eada7b4605c89eb2f1f9fcb5ecc92bcd5f505d74
jeffgreenca <jeff.green.ca@gmail.com>
2019-12-22 20:38:03 -0800

    add n2t-asm cmd

---
+++ cmd/n2t-asm/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"bitbucket.org/jeffgreenca/n2t-asm/internal/pkg/assembler"
+	"bitbucket.org/jeffgreenca/n2t-asm/internal/pkg/lex"
+	"bitbucket.org/jeffgreenca/n2t-asm/internal/pkg/parser"
+)
+
+func main() {
+	r, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	// tokenize from source file
+	var tokens []lex.Token
+	scan := bufio.NewScanner(r)
+	for scan.Scan() {
+		tk, err := lex.Tokenize(scan.Text())
+		check(err)
+		tokens = append(tokens, tk...)
+	}
+
+	// parse tokens
+	prog, err := parser.Parse(tokens)
+	check(err)
+
+	// assemble HACK machine instructions
+	hack, err := assembler.Assemble(prog)
+	check(err)
+
+	fmt.Println(strings.Join(hack, "\n"))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
